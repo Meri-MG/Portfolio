1c58a9fcc46345028747b2ca2a5eeace988d82c8
Meri <mgogi13@freeuni.edu.ge>
2021-10-04 21:37:26 +0400

    Fix linter errors

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const barMenu = document.querySelector('.fa-bars');
-const mobileMenu = document.getElementById('#menu');
+const mobileMenu = document.getElementById('menu');
 const closedMenu = document.querySelector('.fa-times');
-const menuLists = menu.querySelectorAll('.mobile-menu');
+const menuLists = document.querySelectorAll('.mobile-menu');
 
-function show () {
+function show() {
   barMenu.style.display = 'none';
   mobileMenu.classList.add('active');
 }
 
-function close () {
+function close() {
   mobileMenu.classList.remove('active');
   barMenu.style.display = 'block';
 }
