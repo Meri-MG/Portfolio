17a4689851d6a904e62d8301630f733c6dd1a213
Meri <mgogi13@freeuni.edu.ge>
2021-11-13 15:01:13 +0400

    Add content to popup window

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ for (let i = 0; i < menuLists.length; i += 1) {
 }
 
 const projects = {
-  name: 'multi-post stories',
-  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.',
-  img: 'images/popup.png',
+  name: 'Yoga Retreat',
+  description: 'This is my first capstone project in Microverse. A website is about fictional yoga retreat that would be held in Thailand. The Project is responsive and fits all the screen sizes.',
+  img: 'images/speakers.png',
   language1: 'css',
   language2: 'html',
   language3: 'bootstrap',
